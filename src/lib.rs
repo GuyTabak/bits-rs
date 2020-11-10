65cb59d0067de9886710a3fa02a6c01b45dfe719
Guy-PC\Guy <tbk.guy@gmail.com>
2020-11-10 19:06:09 +0200

    add file

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,12 @@ use winapi::um::combaseapi::{CoCreateInstance, CoInitializeEx};
 use winapi::um::commctrl::HRESULT;
 use winapi::um::objbase::COINIT_APARTMENTTHREADED;
 
+
+use url::Url;
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+
+
 ///This will place the address of the service in the 'g_pbcm' variable.
 ///If the service is not running, it will start it.
 fn connect_to_bits() -> Result<*mut IBackgroundCopyManager, String> {
@@ -53,6 +59,24 @@ fn create_bits_job(g_pbcm: *mut IBackgroundCopyManager) -> Result<*mut IBackgrou
     Ok(p_job)
 }
 
+fn add_file(bits_jon: *mut IBackgroundCopyJob, file_url: &str, save_path: &str) -> Result<(), String>{
+    let download_url = Url::parse(file_url).unwrap().to_string();
+    let hr: HRESULT;
+
+    hr = unsafe{ (&mut *bits_jon).AddFile(to_wchar(download_url.as_str()).as_ptr() , to_wchar(save_path).as_ptr())};
+
+    if !SUCCEEDED(hr){
+        return Err(format!("Failed to add file to job, with error code: {}", hr));
+    }
+
+    Ok(())
+}
+
+
+fn to_wchar(str : &str) -> Vec<u16> {
+    OsStr::new(str).encode_wide(). chain(Some(0).into_iter()).collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -66,5 +90,14 @@ mod tests {
     fn test_create_bits_job() {
         let bits_service = connect_to_bits().unwrap();
         create_bits_job(bits_service).unwrap();
+    }
+
+    #[test]
+    fn test_add_file(){
+        // Note, you need to run this test as admin
+        let bits_service = connect_to_bits().unwrap();
+        let bits_job = create_bits_job(bits_service).unwrap();
+        add_file(bits_job, "http://speedtest.tele2.net/", "C:\\temp\\file_name.zip").unwrap();
+
     }
 }
